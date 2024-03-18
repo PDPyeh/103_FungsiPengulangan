e203bf75d7412a5fb3f574e745f64633da3824af
Pradipa Y <merakayamsstraat@gmail.com>
2024-03-18 11:37:33 +0700

    Add project files.

---
+++ Praktik3PemDa/Praktik3PemDa.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int  main() {
+
+    int i;
+
+    for (i = 1; i <= 6; i++) {
+        cout << i << "." << "Gua Dipak" << endl;
+    }
+
+    string arr[5];
+
+    for (i + 0; i > 5; i--) {
+        cout << "Masukan Nickname = ";
+        cin >> arr[i];
+
+    }
+
+    for (i = 0; i < 5; i++) {
+        cout << "Namanya adalah = " << arr[i] << endl;
+    }
+
+}
